3770824d3c78a2769c98b0c879fafe26afcfb2d1
Sofiia-Pukha <sofiia.pukha@gmail.com>
2025-11-22 16:34:43 +0200

    Add worker functions for concurrent actions

--- main.cpp
+++ main.cpp
@@ -44,3 +44,50 @@ void run_action_set(char name, int count)
         f(name, k);
     }
 }
+
+void worker1() 
+{
+    run_action_set('a', count_a);
+    sem_a.release(); 
+    
+    run_action_set('d', count_d);
+    sem_d.release(); 
+    sem_d.release(); 
+}
+
+void worker2() 
+{
+    run_action_set('b', count_b);
+    sem_b.release(); 
+    
+    sem_a.acquire(); 
+    run_action_set('e', count_e);
+    sem_e.release(); 
+}
+
+void worker3() 
+{
+    run_action_set('c', count_c);
+    sem_b.acquire(); 
+    run_action_set('f', count_f);
+    sem_f.release(); 
+    
+    sem_e.acquire();
+    run_action_set('i', count_i);
+    sem_i.release(); 
+}
+
+void worker4() 
+{
+    sem_d.acquire(); 
+    run_action_set('h', count_h);
+    sem_i.acquire();
+    run_action_set('k', count_k);
+}
+
+void worker5() 
+{
+    sem_d.acquire(); 
+    run_action_set('g', count_g);
+    run_action_set('j', count_j);
+}
